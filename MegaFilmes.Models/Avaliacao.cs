cc0192ad16273970018afc7d5982f897f302a90e
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-02-11 21:57:21 -0300

    Modelos: Ator, Diretor, Filme, Gênero e Avaliação

---
+++ MegaFilmes.Models/Avaliacao.cs
@@ -0,0 +1,12 @@
+﻿namespace MegaFilmes.Models;
+
+public class Avaliacao
+{
+    public int AvaliacaoId { get; set; }
+    public int Nota { get; set; }
+    public string? Comentario { get; set; }
+
+    public int FilmeId { get; set; }
+
+    public virtual Filme Filme { get; set; }
+}
---
+++ MegaFilmes.Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿namespace MegaFilmes.Models;
+
+public class Filme
+{
+    public Filme()
+    {
+        Elenco = new HashSet<Ator>();
+        Avaliacoes = new HashSet<Avaliacao>();
+    }
+
+    public int FilmeId { get; set; }
+    public string Titulo { get; set; }
+    public int Ano { get; set; }
+
+    public int DiretorId { get; set; }
+    public int GeneroId { get; set; }
+
+    public virtual Diretor Diretor { get; set; }
+    public virtual Genero Genero { get; set; }
+    public virtual ICollection<Ator> Elenco { get; set; }
+    public virtual ICollection<Avaliacao> Avaliacoes { get; set; }
+
+    public double MediaAvaliacoes => Avaliacoes.Sum(item => item.Nota) / Avaliacoes.Count;
+}
